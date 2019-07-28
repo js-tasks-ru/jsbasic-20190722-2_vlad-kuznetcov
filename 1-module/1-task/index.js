b999c40b7e6627266e8b4e3e5e7910f1c341b0d7
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-07-28 23:06:30 +0300

    merge

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,7 +5,7 @@
  * @returns {number}
  */
 function pow(m, n) {
-  var result;
+  let result;
   m = prompt('Введите число', 1);
   n = prompt('Задайте степень', 1);
   if (isNaN(m) === false && isNaN(n) === false) {
