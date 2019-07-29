15ee4b98ad076185e1364fc0690317a6ec786911
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-07-29 19:59:55 +0300

    correction

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,17 +5,17 @@
  * @returns {number}
  */
 function pow(m, n) {
-  let result;
+  var result;
   m = prompt('Введите число', 1);
   n = prompt('Задайте степень', 1);
   if (isNaN(m) === false && isNaN(n) === false) {
     m = +m;
     n = +n;
-    result = Math.pow(m, n);
+    // result = Math.pow(m, n);
+    result = m**n;
     alert('Ответ: ' + result);
   } else {
     alert('Значения должны быть числовыми');
   }
 }
-
 pow();
