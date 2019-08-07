08247a2b918e24f593f51de8a86620c9de19a367
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-08-07 23:57:33 +0300

    3-module final

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,20 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
-function showSalary(data, age) {
-  // const person = data.filter(data['age'] => data['age'] <= age);
-  // return console.log(person);
+function showSalary(arr, yearsOld) {
+  let result = '';
+  const arrEnd = [];
+  arr.forEach((item) => {
+    const arr1 = [item.name, item.balance];
+    const arr2 = [];
+    if (item.age <= yearsOld) {
+      arr2.push(arr1.join(', '));
+      str = arr2.join();
+      arrEnd.push(str);
+    }
+  });
+  result = arrEnd.join('\n');
+  // console.log(arrEnd);
+  // console.log(result);
+  return result;
 }
