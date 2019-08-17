69d8415d277768c4a071b9927422a5c70e262abf
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-08-17 21:56:07 +0300

    5-module final

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -3,4 +3,35 @@
  * @param {Element} table
  */
 function highlight(table) {
+  const persons = table.tBodies;
+  // console.log(persons);
+  for (const item of persons) {
+    // console.log(item.rows);
+    for (const cell of item.rows) {
+      // console.log(cell.cells);
+      const dataAvailable = cell.cells[3].getAttribute('data-available');
+      const gender = cell.cells[2].innerHTML;
+      const age = cell.cells[1].innerHTML;
+      // console.log(dataAvailable);
+      // console.log(gender);
+      // console.log(age);
+      if (dataAvailable === 'true') {
+        cell.classList.add('available');
+        // console.log(cell);
+      } else if (dataAvailable === 'false') {
+        cell.classList.add('unavailable');
+      } else {
+        cell.hidden = true;
+      }
+      if (gender === 'm') {
+        cell.classList.add('male');
+      } else if (gender === 'f') {
+        cell.classList.add('female');
+      }
+      if (age < 18) {
+        cell.style.textDecoration = 'line-through';
+      }
+      // console.log(cell);
+    }
+  }
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -17,13 +17,123 @@ function SortableTable(items) {
   /**
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
-  this.el = document.createElement('table');
-
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+  const p = document.createElement('p');
+  function getTheadList() {
+    const fragment = new DocumentFragment();
+    const tblHead = ['Name', 'Age', 'Salary', 'City'];
+    for (const item of tblHead) {
+      const td = document.createElement('td');
+      td.textContent = item;
+      fragment.append(td);
+    }
+    return fragment;
+  }
+  thead.append(getTheadList());
+  table.append(thead);
+  /* for (let i = 0; i < 4; i++) {
+    let tr = document.createElement('tr');
+    tr.append(getTheadList());
+    tbody.append(tr);
+  } */
+  items.forEach((person) => {
+    const tr = document.createElement('tr');
+    function getTbodyContent() {
+      const fragment = new DocumentFragment();
+      const tblCont = [person.name, person.age, person.salary, person.city];
+      for (const item of tblCont) {
+        const td = document.createElement('td');
+        td.textContent = item;
+        fragment.append(td);
+      }
+      return fragment;
+    }
+    tr.append(getTbodyContent());
+    tbody.append(tr);
+  });
+  table.append(tbody);
+  this.el = table;
   /**
    * Метод выполняет сортировку таблицы
    * @param {number} column - номер колонки, по которой
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    const tableRows = Array.from(this.el.rows);
+    if (desc === false) {
+      if (column !== 1 && column !== 2) {
+        const sortedRows = tableRows.sort((rowA, rowB) => {
+          // rowA.cells[column].innerHTML > rowB.cells[column].innerHTML ? 1 : -1;
+          if (rowA.cells[column].innerHTML > rowB.cells[column].innerHTML) {
+            return 1;
+          }
+          if (rowA.cells[column].innerHTML === rowB.cells[column].innerHTML) {
+            return 0;
+          }
+          if (rowA.cells[column].innerHTML < rowB.cells[column].innerHTML) {
+            return -1;
+          }
+          return false;
+        });
+        // console.log(sortedRows);
+        this.el.tBodies[0].append(...sortedRows);
+      }
+      if (column === 1 || column === 2) {
+        const sortedRows = tableRows.sort((rowA, rowB) => {
+          // +rowA.cells[column].innerHTML > +rowB.cells[column].innerHTML ? 1 : -1;
+          if (+rowA.cells[column].innerHTML > +rowB.cells[column].innerHTML) {
+            return 1;
+          }
+          if (+rowA.cells[column].innerHTML === +rowB.cells[column].innerHTML) {
+            return 0;
+          }
+          if (+rowA.cells[column].innerHTML < +rowB.cells[column].innerHTML) {
+            return -1;
+          }
+          return false;
+        });
+        // console.log(sortedRows);
+        this.el.tBodies[0].append(...sortedRows);
+      }
+    }
+    if (desc === true) {
+      if (column !== 1 && column !== 2) {
+        const sortedRows = tableRows.sort((rowA, rowB) => {
+          // rowA.cells[column].innerHTML < rowB.cells[column].innerHTML ? 1 : -1;
+          if (rowA.cells[column].innerHTML < rowB.cells[column].innerHTML) {
+            return 1;
+          }
+          if (rowA.cells[column].innerHTML === rowB.cells[column].innerHTML) {
+            return 0;
+          }
+          if (rowA.cells[column].innerHTML > rowB.cells[column].innerHTML) {
+            return -1;
+          }
+          return false;
+        });
+        // console.log(sortedRows);
+        this.el.tBodies[0].append(...sortedRows);
+      }
+      if (column === 1 || column === 2) {
+        const sortedRows = tableRows.sort((rowA, rowB) => {
+          // +rowA.cells[column].innerHTML < +rowB.cells[column].innerHTML ? 1 : -1;
+          if (+rowA.cells[column].innerHTML < +rowB.cells[column].innerHTML) {
+            return 1;
+          }
+          if (+rowA.cells[column].innerHTML === +rowB.cells[column].innerHTML) {
+            return 0;
+          }
+          if (+rowA.cells[column].innerHTML > +rowB.cells[column].innerHTML) {
+            return -1;
+          }
+          return false;
+        });
+        // console.log(sortedRows);
+        this.el.tBodies[0].append(...sortedRows);
+      }
+    }
+  };
 }
